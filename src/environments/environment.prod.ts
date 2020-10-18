15c254424dddcb432521bc103b71c2bc766ca0c7
Gerrit Stapper <gerrit-stapper@gmx.de>
2020-10-18 17:45:56 +0200

    Fix: Swarp Prod API root to assets as well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { AbstractEnvironment } from './abstract-environment';
 
 export const environment: AbstractEnvironment = {
   production: true,
-  apiRoot: ' https://firestore.googleapis.com/v1/projects/cas-scheduler/databases/(default)/documents/lectures'
+  apiRoot: './assets/'
 };
